ff2bb5bdf030ae247f4314ef2a29f8450a58c712
nirhdhd <nirhdhd@gmail.com>
2020-09-17 08:43:12 +0300

    add database and server side

--- src/components/register/register_component.jsx
+++ src/components/register/register_component.jsx
@@ -1,38 +1,59 @@
-import React, { useState } from 'react';
-import Card from '@material-ui/core/Card';
-import './register_component.scss'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
+import Card from "@material-ui/core/Card";
+import "./register_component.scss";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 function Register() {
-    // Declare a new state variable, which we'll call "count"
-    // const [count, setCount] = useState(0);
+  // Declare a new state variable, which we'll call "count"
+  // const [count, setCount] = useState(0);
 
-    return (
+  return (
+    <div className="divStyle">
+      <Card className="cardStyle">
+        <h1>Register</h1>
 
-        <div className="divStyle" >
-            <Card className="cardStyle">
-
-                <h1>Register</h1>
-
-                <div className="foo">
-                    <TextField style={{ width: "350px", margin: 'auto' }} id="outlined-basic" label="username" variant="outlined" type='text' />
-                    <div className="divider" ></div>
-                    <TextField style={{ width: "350px", margin: 'auto' }} id="outlined-basic" label="Email" variant="outlined" type='email' />
-                    <div className="divider" ></div>
-                    <TextField style={{ width: "350px", margin: 'auto' }} id="outlined-basic" label="Password" variant="outlined" type='password' />
-                </div>
-                <div className="divider" ></div>
-
-
-                <div className="foo2">
-                    <Button className="buttonStyle" variant="contained">confirm</Button>
-                    <a className='linkStyle' href="https://reactjs.org/docs/hooks-overview.html">registered allredy? click here </a>
-                </div>
-
-            </Card>
+        <div className="foo">
+          <TextField
+            style={{ width: "350px", margin: "auto" }}
+            id="username"
+            label="username"
+            variant="outlined"
+            type="text"
+          />
+          <div className="divider"></div>
+          <TextField
+            style={{ width: "350px", margin: "auto" }}
+            id="email"
+            label="Email"
+            variant="outlined"
+            type="email"
+          />
+          <div className="divider"></div>
+          <TextField
+            style={{ width: "350px", margin: "auto" }}
+            id="password"
+            label="Password"
+            variant="outlined"
+            type="password"
+          />
         </div>
-    );
+        <div className="divider"></div>
+
+        <div className="foo2">
+          <Button className="buttonStyle" variant="contained">
+            confirm
+          </Button>
+          <a
+            className="linkStyle"
+            href="https://reactjs.org/docs/hooks-overview.html"
+          >
+            registered allredy? click here{" "}
+          </a>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
 export default Register;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 import { StylesProvider } from "@material-ui/core";
 
-import Login from './components/login/login_component.jsx'
-import Register from './components/register/register_component.jsx'
+//components
+import Login from "./components/login/login_component.jsx";
+import Register from "./components/register/register_component.jsx";
+
+//react-router
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <StylesProvider injectFirst>
-      <Register />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      ,
     </StylesProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
